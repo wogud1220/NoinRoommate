5614d12e77b5d306918283ebb24f091ffa9c4d81
JEONG-JIN-GYO <202021035@sangmyung.kr>
2024-11-20 22:12:10 +0900

    Add files via upload

---
+++ pages/main.js
@@ -0,0 +1,9 @@
+document.querySelectorAll('.zoom').forEach(button => {
+    button.addEventListener('click', event => {
+        const isZoomIn = event.target.textContent === '+';
+        const body = document.body;
+        let currentZoom = parseFloat(body.style.zoom) || 1;
+        currentZoom += isZoomIn ? 0.1 : -0.1;
+        body.style.zoom = Math.max(0.5, Math.min(2, currentZoom));
+    });
+});
