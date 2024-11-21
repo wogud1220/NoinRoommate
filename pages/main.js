e1c9885190451a3b0fae8784a10d0dbd6742c511
윤재형 <yoonwogud@gmail.com>
2024-11-21 21:42:07 +0900

    직원 호출 음성 추가

--- pages/main.js
+++ pages/main.js
@@ -1,9 +1,14 @@
-document.querySelectorAll('.zoom').forEach(button => {
-    button.addEventListener('click', event => {
-        const isZoomIn = event.target.textContent === '+';
-        const body = document.body;
-        let currentZoom = parseFloat(body.style.zoom) || 1;
-        currentZoom += isZoomIn ? 0.1 : -0.1;
-        body.style.zoom = Math.max(0.5, Math.min(2, currentZoom));
-    });
+document.querySelectorAll(".zoom").forEach((button) => {
+  button.addEventListener("click", (event) => {
+    const isZoomIn = event.target.textContent === "+";
+    const body = document.body;
+    let currentZoom = parseFloat(body.style.zoom) || 1;
+    currentZoom += isZoomIn ? 0.1 : -0.1;
+    body.style.zoom = Math.max(0.5, Math.min(2, currentZoom));
+  });
 });
+
+function playSiren() {
+  const sirenAudio = document.getElementById("siren-sound");
+  sirenAudio.play();
+}
